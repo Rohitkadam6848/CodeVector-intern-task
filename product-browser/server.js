f8de43055c3e402a49544f14f25540cad01685e4
Rohit Kadam <rohitkadam1230@gmail.com>
2026-06-23 20:03:45 +0530

    Remove .env from repository

--- product-browser/server.js
+++ product-browser/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // Routes
 app.use("/products", productRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
