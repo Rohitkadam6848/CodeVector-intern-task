e1c8c71d8042dc3a7008f869fc4b89bdb3150c69
Rohit Kadam <rohitkadam1230@gmail.com>
2026-06-23 21:14:47 +0530

    Update frontend API URL and backend changes

--- product-browser/db.js
+++ product-browser/db.js
@@ -9,4 +9,9 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
+pool
+  .query("SELECT NOW()")
+  .then(() => console.log("✅ PostgreSQL connected successfully"))
+  .catch((err) => console.error("❌ PostgreSQL connection failed:", err));
+
 module.exports = pool;
